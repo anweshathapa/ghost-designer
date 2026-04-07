681d69eb7180dbf96d043f56c3e11586f9363b87
Anwesha Thapa <anweshathapa15@gmail.com>
2026-04-07 20:15:01 +0530

    Initial Commit (Project Setup)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { DM_Sans } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const appFont=DM_Sans({
+  subsets:['latin']
+})
 
 export const metadata: Metadata = {
   title: "UIUX Mockup generator App",
@@ -23,11 +17,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="en">
+      <body
+      className={appFont.className}
+      >
+      {children}
+      </body>
     </html>
   );
 }
